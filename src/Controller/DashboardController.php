9f50c9420ad379ce1c9f09c67d2ae5c067e996d9
Albert Base <albert.albertbase09675695595@gmail.com>
2025-11-07 21:24:44 +0800

    maayo na

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+// #[IsGranted(attribute: 'ROLE_ADMIN')]
+#[IsGranted(attribute: 'ROLE_USER')]
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(): Response
+    {
+        return $this->render(view: 'base.html.twig', parameters:[]);
+    }
+}
